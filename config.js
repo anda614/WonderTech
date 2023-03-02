77d2a985bd18c68cd61e2bbe4e868f5a48e263cf
DESKTOP-1OMG871\Anda <anda.cozminca@student.usv.ro>
2023-03-02 13:51:44 +0200

    Add existing project files to Git

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {};
+
+config.endpoint = "https://wondertech.documents.azure.com:443/";
+config.key =  "7XsnETHTQ61NXbzgrQHiqXU1PRRF6rIq5xrklhGnVLM4rloWY7mA6KwGuoKHbNwJxsuSuebCnzzFACDbWy4OJQ==";
+config.database = "wondertechDB";
+config.container = "Data";
+
+module.exports = config;
